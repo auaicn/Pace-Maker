ba8ad4a49abc545e911081f7281cce4611aa3a3a
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-04-08 18:05:59 +0900

    model created

---
+++ Pace Maker/Pace Maker/Model/Route.swift
@@ -0,0 +1,13 @@
+//
+//  Route.swift
+//  Pace Maker
+//
+//  Created by Kyungho on 2021/04/08.
+//
+
+import Foundation
+
+struct Route{
+    var distance : UInt
+    var timeSpentInSeconds : UInt
+}
