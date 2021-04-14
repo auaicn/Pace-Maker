d91edaa23d307bd231954d2c16c65acfd8508df1
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-04-15 03:40:13 +0900

    스토리보드 변경, 테이블뷰 구현 준비
    - 테스트를 진행한 기존 뷰 컨트롤러 `KyungHoTableViewController` 제거
    - 메인툴바에 나오는 심볼 크기 조정
    - 테이블뷰를 위한 컨트롤러 생성 및 연결

---
+++ Pace Maker/Pace Maker/ViewController/KDetailedTableCellViewController.swift
@@ -0,0 +1,15 @@
+//
+//  KDetailedTableCellViewController.swift
+//  Pace Maker
+//
+//  Created by Kyungho on 2021/04/15.
+//
+
+import UIKit
+
+class KDetailedTableCellViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Pace Maker/Pace Maker/ViewController/KTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  KTableViewController.swift
+//  Pace Maker
+//
+//  Created by Kyungho on 2021/04/15.
+//
+
+import UIKit
+class KTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
