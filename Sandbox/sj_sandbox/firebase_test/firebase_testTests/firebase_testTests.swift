8e8993b993997cd39a7a67f12576bcc25bb6b89c
ohsungjoon <31716977+ohsungjoon@users.noreply.github.com>
2021-05-07 14:42:13 +0900

    firebase usage example

---
+++ Sandbox/sj_sandbox/firebase_test/firebase_testTests/firebase_testTests.swift
@@ -0,0 +1,33 @@
+//
+//  firebase_testTests.swift
+//  firebase_testTests
+//
+//  Created by 성준오 on 2021/05/06.
+//
+
+import XCTest
+@testable import firebase_test
+
+class firebase_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
