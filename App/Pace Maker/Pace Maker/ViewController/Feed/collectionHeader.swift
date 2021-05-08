1d6de5cf9b76e247b4121a8b20169a0823484f16
YeonJi <jyj9647@naver.com>
2021-05-09 06:55:48 +0900

    Add CollectioHeader

---
+++ App/Pace Maker/Pace Maker/ViewController/Feed/collectionHeader.swift
@@ -0,0 +1,12 @@
+//
+//  collectionHeader.swift
+//  Pace Maker
+//
+//  Created by 전연지 on 2021/05/08.
+//
+
+import UIKit
+
+class collectionHeader: UICollectionReusableView {
+        
+}
---
+++ App/Pace Maker/Pace Maker/ViewController/Feed/feedCollectionHeader.swift
@@ -0,0 +1,12 @@
+//
+//  feedCollectionHeader.swift
+//  Pace Maker
+//
+//  Created by 전연지 on 2021/05/08.
+//
+
+import UIKit
+
+class feedCollectionHeader: UICollectionReusableView {
+        
+}
