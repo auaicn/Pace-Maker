2888a254758104f0d27dd4a6f4401d4e3344bff5
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-06-10 11:48:08 +0900

    Running Setting excluded from Original UI

---
+++ App/Pace Maker/Pace Maker/Model/HapticFeedback.swift
@@ -0,0 +1,11 @@
+//
+//  vibration.swift
+//  
+//
+//  Created by Kyungho on 2021/06/10.
+//
+
+import Foundation
+import UIKit
+
+let hapticfeedbackGenerator = UINotificationFeedbackGenerator()
---
+++ App/Pace Maker/Pace Maker/ViewController/Running/RunningSettingViewController.swift
@@ -0,0 +1,54 @@
+//
+//  RunningSettingViewController.swift
+//  Pace Maker
+//
+//  Created by Kyungho on 2021/06/10.
+//
+
+import UIKit
+
+class RunningSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let gesture = UIPanGestureRecognizer.init(target: self, action: #selector(RunningSettingViewController.panGesture))
+        view.addGestureRecognizer(gesture)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareBackgroundView()
+    }
+    
+    @objc func panGesture(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        let y = self.view.frame.minY
+        self.view.frame = CGRect(x: 0, y: y + translation.y, width: view.frame.width, height: view.frame.width)
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            let frame = self?.view.frame
+            let yComponent = UIScreen.main.bounds.height - 200
+            self?.view.frame = CGRect(x: 0, y: yComponent, width: frame!.width, height: frame!.height)
+        }
+    }
+    
+    func prepareBackgroundView(){
+        let blurEffect = UIBlurEffect.init(style: .dark)
+        let visualEffect = UIVisualEffectView.init(effect: blurEffect)
+        let bluredView = UIVisualEffectView.init(effect: blurEffect)
+        bluredView.contentView.addSubview(visualEffect)
+        
+        visualEffect.frame = UIScreen.main.bounds
+        bluredView.frame = UIScreen.main.bounds
+        
+        view.insertSubview(bluredView, at: 0)
+    }
+
+}
